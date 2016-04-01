19cef83f56c18a83c036746f8703348de83c2083
Alexander Uitz <alexander.uitz@kupferwerk.com>
2016-04-01 14:55:16 +0200

    initial push

--- game.rb
+++ game.rb
@@ -1,3 +1,22 @@
+class Die
+  def roll
+    @numberShowing = 1 + rand(6)
+  end
 
-class 
+  def showing
+    @numberShowing
+  end
 end
+
+BEGIN{
+
+  puts "lets play mäxle!"
+}
+
+die1 = Die.new
+die2 = Die.new
+die1.roll
+die2.roll
+puts die1.showing
+puts die2.showing
+
